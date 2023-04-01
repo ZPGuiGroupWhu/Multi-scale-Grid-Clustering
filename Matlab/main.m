f6e1fd1e322f74b5686cf62060a6650496c15c72
Dehua Peng <2410270794@qq.com>
2023-04-01 14:14:53 +0800

    Add files via upload

---
+++ Matlab/main.m
@@ -0,0 +1,20 @@
+%% Read the raw data
+data = textread('../cluster_data1.txt');
+[n,~] = size(data);
+X = data(:,1:2);
+ref = data(:,3); 
+
+%% Set the number of grid cells
+grid_num = 27;
+
+%% Conduct MSGC algorithm
+result = MSGC(X,grid_num);
+
+%% Visualize the clustering results
+plotcluster(X, result);
+
+[ACC, NMI, ARI, Fscore, JI, RI] = ClustEval(ref, result);
+
+figure(2);
+bar([ACC, NMI, ARI, Fscore, JI, RI]);
+set(gca,'xticklabel',{'ACC','NMI','ARI','Fscore','JI','RI'})
